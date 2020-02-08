5f6653dfa7c3e5330615138a9601a3be12d1e8bf
maxHuzen <49397357+maxHuzen@users.noreply.github.com>
2020-02-08 23:47:14 +0100

    Bruh

--- BruhBoy/BruhBoy/Character.cs
+++ BruhBoy/BruhBoy/Character.cs
@@ -1,27 +1,28 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Timers;
 
 namespace BruhBoy
 {
     public abstract class Character
     {
-        public bool IsJumping { get; set; }
+        private float ground { get; set; } = 400;
+        Timer JumpTimer;
+        public bool IsJumping { get; set; } = false;
         public Texture2D Texture { get; set; }
         public Vector2 Position { get; set; }
         public Direction FacingDirection { get; set; }
 
         public Character()
         {
-            IsJumping = false;
             FacingDirection = Direction.Right;
-
         }
         public void Gravity()
         {
-            if (this.Position.Y < 400)
+            if (Position.Y != ground)
             {
-                Position = ChangePos(this.Position.X, this.Position.Y + 3);
+                Position = ChangePos(Position.X, Position.Y + 10);
             }
         }
         public Vector2 ChangePos(float X, float Y)
@@ -33,7 +34,7 @@ namespace BruhBoy
         }
         public void Movement()
         {
-            if (Keyboard.GetState().IsKeyDown(Keys.W) && Position.Y == 402)
+            if (Keyboard.GetState().IsKeyDown(Keys.W) && Position.Y == ground && IsJumping == false)
             {
                 Jump();
             }
@@ -50,10 +51,22 @@ namespace BruhBoy
         }
         public void Jump()
         {
-            while (!IsJumping)
-            {
+            IsJumping = true;
+            SetTimer(100);
 
-            }
+        }
+
+        public void SetTimer(int interval)
+        {
+            JumpTimer = new Timer(interval);
+            JumpTimer.Elapsed += OnTimerElapsed;
+            JumpTimer.AutoReset = true;
+            JumpTimer.Enabled = true;
+        }
+
+        public void OnTimerElapsed(object source, ElapsedEventArgs e)
+        {
+
         }
     }
 }
